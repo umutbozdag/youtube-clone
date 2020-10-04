63c45e5064b2831c406d6fbb6fd7a9a32565f5d4
Umut Bozdağ <umutbozdag@outlook.com.tr>
2020-10-04 23:04:01 +0300

    feat: update app to reuse components

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,19 @@
 <template>
   <div id="app">
-    <router-view />
+    <navbar />
+    <div class="sidebar-main-wrapper" style="display: flex;">
+      <sidebar />
+      <router-view class="route-wrapper"></router-view>
+    </div>
   </div>
 </template>
+
+<script>
+import Sidebar from "@/components/sidebar";
+import Navbar from "@/components/navbar";
+export default {
+  components: { Sidebar, Navbar },
+};
+</script>
 
 <style lang="scss"></style>
