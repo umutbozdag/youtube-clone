bfaa84f1a5929a3793e93c74992aaf46a9cd2078
Umut Bozdağ <umutbozdag@outlook.com.tr>
2020-10-04 23:00:38 +0300

    feat: add search input component

---
+++ src/components/search-input.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="search-input row">
+    <input class="search-input__field" type="text" placeholder="Search" />
+    <icon-search class="search-input__icon" />
+  </div>
+</template>
+
+<script>
+import IconSearch from "@/assets/icons/icon-search.svg";
+export default {
+  components: {
+    IconSearch,
+  },
+};
+</script>
+
+<style></style>
