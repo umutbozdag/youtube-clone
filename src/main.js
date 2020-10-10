825f6d2ee7a300020bc28540ebc073edef03ace1
Umut Bozdağ <umutbozdag@outlook.com.tr>
2020-10-10 13:35:43 +0300

    feat: add global component

--- src/main.js
+++ src/main.js
@@ -5,6 +5,9 @@ import router from "./router";
 import store from "./store";
 import "@/styles/main.scss";
 
+import BaseButton from "@/components/base-button";
+
+Vue.component("base-button", BaseButton);
 Vue.config.productionTip = false;
 
 new Vue({
