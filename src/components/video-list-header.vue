a6ff84e08aa2407ea1ae5719c01f57f6c4d924e9
Umut Bozdağ <umutbozdag@outlook.com.tr>
2020-10-10 13:36:05 +0300

    feat: add video-list components

---
+++ src/components/video-list-header.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="video-list-header">
+    <div class="video-list-header__content">
+      <slot name="content"></slot>
+    </div>
+
+    <div class="video-list-header__controls">
+      <slot name="extra-control"></slot>
+      <base-button
+        variant="secondary"
+        @click="$emit('swipe-left')"
+        rounded
+        as-icon
+        styles="marginRight: 10px;"
+      >
+        <icon-left-arrow />
+      </base-button>
+      <base-button
+        variant="secondary"
+        @click="$emit('swipe-right')"
+        rounded
+        as-icon
+      >
+        <icon-right-arrow />
+      </base-button>
+    </div>
+  </div>
+</template>
+
+<script>
+import IconLeftArrow from "@/assets/icons/icon-left-arrow.svg";
+import IconRightArrow from "@/assets/icons/icon-right-arrow.svg";
+
+export default {
+  name: "VideoListHeader",
+  components: {
+    IconLeftArrow,
+    IconRightArrow,
+  },
+};
+</script>
+
+<style></style>
---
+++ src/components/video-list.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="video-list">
+    <div class="video-list__videos" ref="videos" :id="id">
+      <video-card
+        v-for="video in videos"
+        :key="video.id"
+        :title="video.title"
+        :owner="video.owner"
+        :time="video.time"
+        :duration="video.duration"
+        :views="video.views"
+        :size="video.size"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+import VideoCard from "./video-card";
+
+export default {
+  components: {
+    VideoCard,
+  },
+  props: {
+    videos: {
+      type: Array,
+    },
+    id: {
+      type: String,
+    },
+  },
+};
+</script>
+
+<style></style>
