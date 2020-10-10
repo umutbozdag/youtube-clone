8150b41daaaf2d904f06e85fa35ddea1aee01b88
Umut Bozdağ <umutbozdag@outlook.com.tr>
2020-10-10 13:32:30 +0300

    feat: add button and button-group components

---
+++ src/components/base-button-group.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="button-group">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "BaseButtonGroup",
+};
+</script>
+
+<style></style>
---
+++ src/components/base-button.vue
@@ -0,0 +1,50 @@
+<template>
+  <button
+    class="button"
+    @click="$emit('click')"
+    :class="{
+      'button--primary': variant === 'primary',
+      'button--light': variant === 'light',
+      'button--secondary': variant === 'secondary',
+      'button--rounded': rounded,
+      'button--as-icon': asIcon,
+      'button--transparent': transparent,
+    }"
+    :style="styles"
+  >
+    <slot></slot>
+  </button>
+</template>
+
+<script>
+export default {
+  name: "BaseButton",
+  props: {
+    variant: {
+      type: String,
+      required: true,
+    },
+
+    styles: {
+      type: String,
+    },
+
+    rounded: {
+      type: Boolean,
+      default: false,
+    },
+
+    asIcon: {
+      type: Boolean,
+      default: false,
+    },
+
+    transparent: {
+      type: Boolean,
+      default: false,
+    },
+  },
+};
+</script>
+
+<style></style>
