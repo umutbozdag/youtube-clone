1b46bc4aaa0053b3e8049d3ae6f01e185052b9af
Umut Bozdağ <umutbozdag@outlook.com.tr>
2020-10-10 13:35:54 +0300

    feat: update video-card component

---
+++ src/components/video-card.vue
@@ -0,0 +1,63 @@
+<template>
+  <div
+    @click="$router.push('/watch')"
+    class="video-card"
+    :class="{ 'video-card--big': size === 'big' }"
+  >
+    <div class="video-card__cover">
+      <img
+        v-if="size !== 'big'"
+        class="video-card-thumbnail"
+        src="../assets/images/video-cover.png"
+        :alt="title"
+        tabindex="0"
+      />
+
+      <img
+        v-else
+        src="../assets/images/video-cover-big.png"
+        tabindex="0"
+        :alt="title"
+      />
+      <span class="video-card__duration">{{ duration }}</span>
+    </div>
+    <div class="video-card__summary">
+      <h3 class="video-title">{{ title }}</h3>
+      <div class="video-meta">
+        <p class="video-meta__info">{{ views }} · {{ time }}</p>
+        <p class="video-meta__owner">{{ owner }}</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "VideoItem",
+  props: {
+    size: {
+      type: String,
+    },
+    videoSrc: {
+      type: String,
+    },
+    duration: {
+      type: String,
+    },
+    title: {
+      type: String,
+    },
+    owner: {
+      type: String,
+    },
+    time: {
+      type: String,
+    },
+    views: {
+      type: String,
+    },
+  },
+};
+</script>
+
+<style></style>
