702a7aa9cd2738f44a1979219d3975a26930c05c
Umut Bozdağ <umutbozdag@outlook.com.tr>
2020-10-11 17:33:38 +0300

    feat: add icons as global components

--- src/main.js
+++ src/main.js
@@ -5,9 +5,21 @@ import router from "./router";
 import store from "./store";
 import "@/styles/main.scss";
 
+import IconHome from "@/assets/icons/icon-home.svg";
+import IconMenu from "@/assets/icons/icon-menu.svg";
+import IconCamera from "@/assets/icons/icon-camera.svg";
+import IconGrid from "@/assets/icons/icon-grid.svg";
+import IconNotification from "@/assets/icons/icon-notification.svg";
+
 import BaseButton from "@/components/base-button";
 
 Vue.component("base-button", BaseButton);
+Vue.component("icon-home", IconHome);
+Vue.component("icon-menu", IconMenu);
+Vue.component("icon-camera", IconCamera);
+Vue.component("icon-grid", IconGrid);
+Vue.component("icon-notification", IconNotification);
+
 Vue.config.productionTip = false;
 
 new Vue({
