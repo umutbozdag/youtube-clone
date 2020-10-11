d7dd3654b7fc6433804cb87a1774ec06bf880fa9
Umut Bozdağ <umutbozdag@outlook.com.tr>
2020-10-11 17:31:27 +0300

    feat: add navbar mobile component

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,9 @@
 <template>
   <div id="app">
     <navbar />
-    <div class="sidebar-main-wrapper" style="display: flex;">
+    <div class="sidebar-main-wrapper grid" style="display: flex;">
       <the-sidebar />
+      <navbar-mobile />
       <router-view class="route-wrapper"></router-view>
     </div>
   </div>
@@ -11,10 +12,13 @@
 <script>
 import TheSidebar from "@/components/the-sidebar";
 import Navbar from "@/components/navbar";
+import NavbarMobile from "@/components/navbar-mobile";
+
 export default {
   components: {
     TheSidebar,
     Navbar,
+    NavbarMobile,
   },
 };
 </script>
---
+++ src/components/navbar-mobile.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="navbar__mobile">
+    <ul class="mobile-menu">
+      <li class="mobile-menu__item">
+        <icon-home />
+        <p class="mobile-menu__item-name">Home</p>
+      </li>
+      <li class="mobile-menu__item">
+        <icon-home />
+        <p class="mobile-menu__item-name">Trending</p>
+      </li>
+      <li class="mobile-menu__item">
+        <icon-home />
+        <p class="mobile-menu__item-name">Subscriptions</p>
+      </li>
+      <li class="mobile-menu__item">
+        <icon-home />
+        <p class="mobile-menu__item-name">Library</p>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
