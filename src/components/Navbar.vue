06a883c6e0aab1a10686dd3001e8ebc66e1506be
Umut Bozdağ <umutbozdag@outlook.com.tr>
2020-10-11 22:00:03 +0300

    feat: update navbar component

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,28 +1,61 @@
 <template>
-  <div class="navbar">
+  <div
+    class="navbar"
+    :class="{ 'navbar--channel-detail': $route.name === 'ChannelDetail' }"
+  >
     <div class="navbar__container">
-      <div class="navbar__brand">
+      <div class="navbar__brand row">
         <icon-menu @click="handleOnMenuClicked" class="menu-icon" />
-        <img class="brand-logo" src="@/assets/logo.png" alt="YouTube" />
+        <img
+          class="brand-logo"
+          src="@/assets/logo-big.png"
+          alt="YouTube logo"
+        />
+        <template v-if="$route.name === 'ChannelDetail'">
+          <img
+            src="@/assets/logo-small.png"
+            class="brand-logo--small"
+            alt="Youtube small logo"
+          />
+          <h2 class="navbar__user-name">Margaret Phelps</h2>
+        </template>
       </div>
       <div class="navbar__search">
-        <base-input show-icon styles="backgroundColor: red; width: 800px;" />
+        <base-input show-icon />
       </div>
 
       <div class="navbar__controls">
         <ul class="controls-list">
           <li class="controls-list__item">
-            <icon-camera />
+            <icon-camera class="controls-item-icon" />
           </li>
           <li class="controls-list__item">
-            <icon-grid />
+            <icon-grid class="controls-item-icon" />
           </li>
           <li class="controls-list__item">
-            <icon-notification />
+            <icon-notification class="controls-item-icon" />
           </li>
           <li class="controls-list__item">
             <img
-              class="list-item-avatar"
+              class="controls-item-avatar"
+              src="../assets/images/avatar-user.png"
+              loading="lazy"
+              alt=""
+            />
+          </li>
+        </ul>
+      </div>
+
+      <div class="navbar__controls--mobile">
+        <ul class="controls-list--mobile row">
+          <li class="controls-list__item">
+            <icon-search
+              class="controls-item-icon controls-item-icon--search"
+            />
+          </li>
+          <li class="controls-list__item">
+            <img
+              class="controls-item-avatar"
               src="../assets/images/avatar-user.png"
               loading="lazy"
               alt=""
