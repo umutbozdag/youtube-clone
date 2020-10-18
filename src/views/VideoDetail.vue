7098accaa2b555d9b565f938b6dad65f6c893f3f
Umut Bozdağ <umutbozdag@outlook.com.tr>
2020-10-19 00:31:23 +0300

    feat: update video detail page

--- src/views/VideoDetail.vue
+++ src/views/VideoDetail.vue
@@ -1,9 +1,109 @@
 <template>
-  <div>video detail</div>
+  <div class="video-detail">
+    <div class="video-detail__content">
+      <iframe
+        id="ytplayer"
+        type="text/html"
+        width="1100"
+        height="700"
+        src="http://www.youtube.com/embed/M7lc1UVf-VE?autoplay=1&origin=http://example.com"
+        frameborder="0"
+      />
+    </div>
+
+    <div class="video-detail__queue">
+      <video-list :videos="userVideos" id="queueVideos" vertical />
+    </div>
+  </div>
 </template>
 
 <script>
-export default {};
+import VideoList from "@/components/VideoList";
+
+export default {
+  name: "VideoDetail",
+  components: {
+    VideoList,
+  },
+  data() {
+    return {
+      userVideos: [
+        {
+          id: 0,
+          title: "A Brief History Of Creation",
+          owner: "Dollie Blair",
+          time: "3 days ago",
+          duration: "4:15",
+          views: "80k views",
+        },
+        {
+          id: 1,
+          title: "A Brief History Of Creation",
+          owner: "Dollie Blair",
+          time: "3 days ago",
+          duration: "4:15",
+          views: "80k views",
+        },
+        {
+          id: 2,
+          title: "A Brief History Of Creation",
+          owner: "Dollie Blair",
+          time: "3 days ago",
+          duration: "4:15",
+          views: "80k views",
+        },
+        {
+          id: 3,
+          title: "A Brief History Of Creation",
+          owner: "Dollie Blair",
+          time: "3 days ago",
+          duration: "4:15",
+          views: "80k views",
+        },
+        {
+          id: 4,
+          title: "A Brief History Of Creation",
+          owner: "Dollie Blair",
+          time: "3 days ago",
+          duration: "4:15",
+          views: "80k views",
+        },
+        {
+          id: 4,
+          title: "A Brief History Of Creation",
+          owner: "Dollie Blair",
+          time: "3 days ago",
+          duration: "4:15",
+          views: "80k views",
+        },
+        {
+          id: 4,
+          title: "A Brief History Of Creation",
+          owner: "Dollie Blair",
+          time: "3 days ago",
+          duration: "4:15",
+          views: "80k views",
+        },
+        {
+          id: 4,
+          title: "A Brief History Of Creation",
+          owner: "Dollie Blair",
+          time: "3 days ago",
+          duration: "4:15",
+          views: "80k views",
+        },
+        {
+          id: 4,
+          title: "A Brief History Of Creation",
+          owner: "Dollie Blair",
+          time: "3 days ago",
+          duration: "4:15",
+          views: "80k views",
+        },
+      ],
+    };
+  },
+};
 </script>
 
 <style></style>
